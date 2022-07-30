e6cfe4c26cb0ffff9967ed3b9af7f85b3f3bb5ee
Vijaythite1 <vijay.thite@solytics-partners.com>
2022-07-30 11:06:47 +0530

    Warranty changes

--- contracts/NFTWarranty.sol
+++ contracts/NFTWarranty.sol
@@ -6,35 +6,32 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract NFTWarrenty is ERC721URIStorage{
-
+contract NFTWarrenty is ERC721URIStorage {
     address payable issuer;
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
-     
-    constructor() ERC721("NFTWarrenty", "NFW"){
+
+    constructor() ERC721("NFTWarrenty", "NFW") {
         issuer = payable(msg.sender);
     }
-    
-    struct TokenInfo{
+
+    struct TokenInfo {
         /* Pertinent entities */
-        address payable owner;  //Buyer
+        address payable owner; //Buyer
         address payable issuer; //Seller
-        
         /* Warrenty Details */
-        string serial_number; 
-        uint256 issue_time; 
+        string serial_number;
+        uint256 issue_time;
         uint256 warranty_duration; //In days
         string link_to_warrenty_condition;
-
-        //Warrenty mechanics        
+        //Warrenty mechanics
         uint256 num_transfers_allowed;
     }
 
     // struct TransferInfo{
     //     address payable from_;
     //     address payable to_;
-    //     uint256 token_id; 
+    //     uint256 token_id;
     // }
 
     // struct TokenMetaData{
@@ -42,11 +39,38 @@ contract NFTWarrenty is ERC721URIStorage{
     //     TokenInfo TokenInfo;
     // }
 
-
-    // mapping(uint256 => TokenMetaData) private TokenMetaDataStorage; //Mapping of token id with token meta data 
-
+    // mapping(uint256 => TokenMetaData) private TokenMetaDataStorage; //Mapping of token id with token meta data
+    // mapping(address => _tokenIds[]) private addressToTokenID;
     mapping(uint256 => TokenInfo) private idToTokenInfo; //Mapping of token id with token info
-    
+
+    /* Returns only items that a user has purchased */
+    function fetchMyNFTs(address payable ownerAddress) public view returns (TokenInfo[] memory) {
+        uint256 totalItemCount = _tokenIds.current();
+        uint256 itemCount = 0;
+        uint256 currentIndex = 0;
+
+        for (uint256 i = 0; i < totalItemCount; i++) {
+            if (idToTokenInfo[i + 1].owner == ownerAddress) {
+                itemCount += 1;
+            }
+        }
+
+        TokenInfo[] memory items = new TokenInfo[](itemCount);
+        for (uint256 i = 0; i < totalItemCount; i++) {
+            if (idToTokenInfo[i + 1].owner == ownerAddress) {
+                uint256 currentId = i + 1;
+                TokenInfo storage currentItem = idToTokenInfo[currentId];
+                items[currentIndex] = currentItem;
+                currentIndex += 1;
+            }
+        }
+        return items;
+    }
+    //Delete a NFT 
+    function Burn(uint256 token_Id) private {
+        super._burn(token_Id);
+        delete idToTokenInfo[token_Id];
+    }
 
     /*
         Check Whether a warranty NFT has not expired.
@@ -54,36 +78,64 @@ contract NFTWarrenty is ERC721URIStorage{
         else retuerns true if not expired
     */
 
-    function is_not_expired(uint256 issue_time , uint256 warranty_duration) public view returns (bool) {
-        uint256 one_day =  86400;
-        if(block.timestamp < (issue_time + (warranty_duration * one_day) )){
+    function is_not_expired(
+        uint256 issue_time,
+        uint256 warranty_duration,
+        uint256 token_id
+    ) public returns (bool) {
+        uint256 one_day = 86400;
+        if (block.timestamp < (issue_time + (warranty_duration * one_day))) {
             return false;
-        }
-        else 
-        {
+        } else {
+            Burn(token_id);
             return true;
         }
     }
 
-    function TransferNFT(address payable new_owner, uint256 token_id) public returns (uint256) {
-            // require(if(!idToTokenInfo[token_id]),"No NFT found for this token id")
-            TokenInfo storage info = idToTokenInfo[token_id];
-            require(is_not_expired(info.issue_time,info.warranty_duration) == true,"Warrenty Expired No transfer allowed");
-            _transfer(info.owner,new_owner,token_id);
-            info.issuer = info.owner;
-            info.owner = new_owner;
-            uint256 nta= info.num_transfers_allowed;
-            nta--;
-            info.num_transfers_allowed=nta;
-            return token_id;
+    function checkValidity(uint256 tokenId) public returns (bool) {
+        TokenInfo storage info = idToTokenInfo[tokenId];
+        return (
+            is_not_expired(info.issue_time, info.warranty_duration, tokenId)
+        );
     }
 
-    function createListedToken(uint256 currentTokenId, address payable owner,string memory serial_number, uint256 warranty_duration,string memory link_to_warranty_condition,uint256 num_transfers_allowed) private 
+    /* 
+         Transfer NFT From One owner to Another
+    */
+    function TransferNFT(address payable old_owner,address payable new_owner, uint256 token_id)
+        public
+        returns (uint256)
     {
-        uint issue_time = block.timestamp;
+        require(idToTokenInfo[token_id].owner == address(0),"No NFT found for this token id");
+        TokenInfo storage info = idToTokenInfo[token_id];
+        require(
+            is_not_expired(info.issue_time, info.warranty_duration, token_id) ==
+                true,
+            "Warrenty Expired No transfer allowed"
+        );
+        require(info.owner != old_owner,"Only Owner can Transfer the Warranty");
+        _transfer(info.owner, new_owner, token_id);
+        info.issuer = info.owner;
+        info.owner = new_owner;
+        uint256 nta = info.num_transfers_allowed;
+        nta--;
+        info.num_transfers_allowed = nta;
+        return token_id;
+    }
+
+    function createListedToken(
+        uint256 currentTokenId,
+        address payable owner,
+        address payable seller,
+        string memory serial_number,
+        uint256 warranty_duration,
+        string memory link_to_warranty_condition,
+        uint256 num_transfers_allowed
+    ) private {
+        uint256 issue_time = block.timestamp;
         idToTokenInfo[currentTokenId] = TokenInfo(
             payable(owner),
-            payable(msg.sender),
+            payable(seller),
             serial_number,
             issue_time,
             warranty_duration,
@@ -91,19 +143,37 @@ contract NFTWarrenty is ERC721URIStorage{
             num_transfers_allowed
         );
 
-        _transfer(msg.sender,owner,currentTokenId);
+        _transfer(seller, owner, currentTokenId);
     }
 
+    /*
+        Create Token BY Seller to Receiver
+    */
 
-    function createToken (string memory tokenURI,address payable owner,string memory serial_number,uint256 warranty_duration,string memory link_to_warranty_condition,uint256 num_transfers_allowed) public payable returns (uint) {
-        //Validations if any 
+    function createToken(
+        string memory tokenURI,
+        address payable seller,
+        address payable owner,
+        string memory serial_number,
+        uint256 warranty_duration,
+        string memory link_to_warranty_condition,
+        uint256 num_transfers_allowed
+    ) public payable returns (uint256) {
+        //Validations if any
         _tokenIds.increment();
         uint256 currentTokenId = _tokenIds.current();
-        _safeMint(msg.sender,currentTokenId);
+        _safeMint(seller, currentTokenId);
         _setTokenURI(currentTokenId, tokenURI);
-       
-        createListedToken(currentTokenId,owner,serial_number,warranty_duration,link_to_warranty_condition,num_transfers_allowed);
+
+        createListedToken(
+            currentTokenId,
+            owner,
+            seller,
+            serial_number,
+            warranty_duration,
+            link_to_warranty_condition,
+            num_transfers_allowed
+        );
         return currentTokenId;
     }
-
 }
